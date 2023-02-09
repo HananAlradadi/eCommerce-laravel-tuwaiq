be032e0a187851f161dc01d9745bb4c6ab0a3d35
HananAlradadi <87634898+HananAlradadi@users.noreply.github.com>
2023-02-09 21:07:59 +0300

    Update index.blade.php

--- resources/views/phone/index.blade.php
+++ resources/views/phone/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div class="text-center">
-    <h1>أهلاً بكم في متجر التكنولوجيا الحديثة لبيع الهواتف</h1>
+    <h1>أهلاً بكم في متجر لبيع الهواتف</h1>
     <h1><a class="btn btn-success" href="{{ route('getPhones') }}">استعراض الهواتف الجديدة</a></h1>
     <h1><a class="btn btn-primary" href="{{ route('oldPhones') }}">استعراض الهواتف القديمة</a></h1>
 </div>
